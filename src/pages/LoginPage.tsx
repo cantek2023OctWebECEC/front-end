94c2b3c8c1001efc24d74f0efd69165f849b4bde
LAPTOP-E72SIBNV\CoCo Yu <coco2023.ca@gmail.com>
2023-10-19 21:35:50 +0800

    Bug fix : success to submit the data

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,18 +4,15 @@ import {useForm} from 'react-hook-form'
 interface FormData{
     email:string,
     password:string,
-    remeber:boolean
+    remember:boolean
 }
 
 export const LoginPage:React.FC = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const {register, handleSubmit} = useForm<FormData>();
 
-    const onSubmit = handleSubmit( ({email,password,remeber}) => {
-        console.log('hello : ');
-        console.log(email,password,remeber);
-    })
+    const onSubmit = handleSubmit((formData) => {
+        console.log(formData.email, formData.password, formData.remember);
+    });
 
 	return (
 		<div className="min-h-screen bg-grey-50 flex flex-col justify-center">
@@ -29,14 +26,14 @@ export const LoginPage:React.FC = () => {
                         <form action="" className='space-y-6' onSubmit={onSubmit}>
                             <div>
                                 <label  className='text-sm font-blod text-gray-600 block'>Email</label>
-                                <input type="text" {...register} onChange={(e) => { setEmail(e.target.value); }}  name="email" value={email}  className='w-full p-2 border border-gray-300 rounded mt-1'/>
+                                <input type="text" {...register('email')}   name="email"  className='w-full p-2 border border-gray-300 rounded mt-1'/>
                             </div>
                             <div>
                                 <label  className='text-sm font-blod text-gray-600 block'>Password</label>
-                                <input type="password" {...register} onChange={(e) => { setPassword(e.target.value); }} name="password" value={password}  className='w-full p-2 border border-gray-300 rounded mt-1'/>
+                                <input type="password" {...register('password')}  name="password"   className='w-full p-2 border border-gray-300 rounded mt-1'/>
                             </div>
                             <div>
-                                <input type="checkbox" {...register} name="remeber" className='h-4 w-4 text-blue-300 rounded'/>
+                                <input type="checkbox" {...register('remember')} name="remember" className='h-4 w-4 text-blue-300 rounded'/>
                                 <label  className='ml-2 text-sm text-gray-600'>Remeber me</label>
                             </div>
                             <div>

0eb72fce3ca3ffce0311607f3c7aa150b4536a0a
Coco2023ca <coco2023.ca@gmail.com>
2023-10-27 22:02:36 +0800

    home page redirect

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,10 @@ import { Login } from "../redux/actions/authAction";
 import { TripCard } from "../components/TripCard";
 import { Button } from "@mui/material";
 import { AddTripForm } from "../components/AddTripForm";
+import {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+
+
 export const HomePage = () => {
 	//used for select trip, create trip, enter trip, delete trip
 	// const { data } = useQuery({
@@ -11,22 +15,32 @@ export const HomePage = () => {
 	// 	queryFn: [],
 	// });
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 	const { userinfo, token, loggedin } = useSelector(
 		(state: RootState) => state.Auth
 	);
+
+
+	useEffect(() => {
+		if (!loggedin) {
+			navigate("/login");
+		}
+	}, [loggedin, navigate]);
+
 	const refresh = () => {
 		if (token) {
 			const [email, password] = atob(token).split(":");
 			dispatch(Login(email, password));
 		}
 	};
-	if (!loggedin) {
-		<div>
-			<p>login to view trips</p>
-		</div>;
-	}
+	
 	return (
 		<>
+		{(!loggedin)?
+		<div>
+			<p>login to view trips</p>
+		</div>:
+			
 			<div className="flex flex-col gap-2 p-2">
 				<div className="flex">
 					<Button
@@ -74,6 +88,7 @@ export const HomePage = () => {
 					</div>
 				</div>
 			</div>
+			}
 		</>
 	);
 };
